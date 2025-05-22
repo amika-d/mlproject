8e89603f792610f1ff0b0e545805202aaaa683b3
amika-d <amika.g.d@gmail.com>
2025-05-22 19:19:07 +0530

    Model Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -87,10 +87,10 @@ class DataTransformation:
             
             
             train_arr = np.c_[
-                input_feature_train_arr, np.array(input_feature_train_df)
+                input_feature_train_arr, np.array(target_feature_train_df)
             ]
             test_arr = np.c_[
-                input_feature_test_arr, np.array(input_feature_test_df)
+                input_feature_test_arr, np.array(target_feature_test_df)
             ]
             logger.info("Saved preprocessing object")
             save_object(
